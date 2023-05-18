28315d3bd9d47294cfe622dff8b537227f67cf38
Ryan Gabriel <ryan.bromati@hotmail.com>
2023-05-18 16:56:21 -0300

    ex005 using fifo

--- listas-dinamicas-estaticas/ex000/ex005.c
+++ listas-dinamicas-estaticas/ex000/ex005.c
@@ -1,50 +1,62 @@
-/* 5. Elabore um algoritmo que leia uma sequência de números, armazenando-os numa lista simplesmente encadeada e que, em seguida,
-verifique se a lista linear encadeada está ordenada. Suponha que a lista não contenha números repetidos. */
+/* 5. Elabore um algoritmo que leia uma sequência de números, armazenando-os numa lista simplesmente encadeada e que, em seguida, verifique se a lista linear encadeada está ordenada. Suponha que a lista não contenha números repetidos. */
 
 #include <stdio.h>
 #include <stdlib.h>
 
 typedef struct lista {
-   int numero;
+   int num;
    struct lista *prox;
 } LISTA;
 
-LISTA* inserirNumero(int novoNumero, LISTA *listaAtual) {
+// USANDO CONCEITO DE PILHA (LIFO)
+LISTA *inserir1(LISTA *lista, int n) {
    LISTA *novaLista = (LISTA *)malloc(sizeof(LISTA));
-   novaLista->numero = novoNumero;
-   novaLista->prox = listaAtual;
+   novaLista->num = n;
+   novaLista->prox = lista;
+   lista = novaLista;
    return novaLista;
 }
 
-void verificarOrdenacao(LISTA *lista) {
-   while(lista != NULL && lista->prox != NULL) {
-      
-      if(lista->numero < lista->prox->numero) {
-         printf("lista não esta ordenada");
-         return;
-      }
+// USANDO CONCEITO DE FILA (FIFO)
+LISTA *inserir2(LISTA *lista, int n) {
+   LISTA *novaLista = (LISTA *)malloc(sizeof(LISTA));
+   LISTA *aux;
+   novaLista->num = n;
+   novaLista->prox = NULL;
 
-      lista = lista->prox;
+   if (lista == NULL) 
+      lista = novaLista;
+   else {
+      aux = lista;
+      while (aux->prox != NULL)
+         aux = aux->prox;
+
+      aux->prox = novaLista;
    }
-   printf("lista esta ordenada");
+   return lista;
+}
+
+LISTA *imprimir(LISTA *lista) {
+   if (lista != NULL) {
+      printf("%i  ", lista->num);
+      return imprimir(lista->prox);
+   }
 }
 
 int main(void) {
    system("clear");
 
    LISTA *listaAtual = NULL;
-   int qntNumero, i, numero;
+   int numero;
 
-   printf("Digite a quantidade de numeros: ");
-   scanf("%i", &qntNumero);
-
-   for (i = 0; i < qntNumero; i++) {
-      printf("Digite o %i numero: ", i + 1);
+   do {
+      printf("Digite os numeros (-1 para parar): ");
       scanf("%i", &numero);
+      if (numero != -1)
+         listaAtual = inserir1(listaAtual, numero);
+   } while (numero != -1);
 
-      listaAtual = inserirNumero(numero, listaAtual);
-   }
-   verificarOrdenacao(listaAtual);
+   imprimir(listaAtual);
 
    return 0;
 }
